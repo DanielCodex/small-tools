a40dc75c438247e058e82561d1dc774247dd1df7
DanielCodex <danialups1997@gmail.com>
2022-09-28 11:56:42 +0200

    don't show data, if there is no username

--- src/components/githubRepo/Card.tsx
+++ src/components/githubRepo/Card.tsx
@@ -17,58 +17,66 @@ function Card({
   following,
 }: Props) {
   return (
-    <div className="card card-side border-r-2 border-orange-400/30 bg-base-200 shadow-xl">
-      <figure>
-        <img className="h-[250px] w-[250px]" src={avatar} alt="Movie" />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title text-2xl text-orange-400 underline decoration-orange-400/30 underline-offset-2">
-          {login}
-        </h2>
-        <p className="leading-6">
-          <span>Blog: </span>
-          {blog ? (
-            <a
-              className="link link-primary text-orange-400 underline decoration-orange-400/30 underline-offset-1"
-              href={blog}
-            >
-              {blog}
-            </a>
-          ) : (
-            <span className="text-slate-400">No Blog</span>
-          )}
-          <br />
-          <span>Twitter: </span>
-          {twitter_username ? (
-            <a
-              className="link link-primary  text-orange-400 underline decoration-orange-400/30 underline-offset-1"
-              href={`https://twitter.com/${twitter_username}`}
-            >
-              @{twitter_username}
-            </a>
-          ) : (
-            <span>NO twitter</span>
-          )}
-          <br />
-          <span>Followers: </span>
-          <span>{followers}</span>
-          <br />
-          <span>Following: </span>
-          <span>{following}</span>
-        </p>
-        <div className="card-actions justify-end">
-          <a
-            href={`https://github.com/${login}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <button className="btn-primary btn btn-outline">
-              Github Profile
-            </button>
-          </a>
+    <>
+      {!login ? (
+        <div className="flex justify-center">
+          <span className="text-9xl">🥺</span>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className="card card-side border-r-2 border-orange-400/30 bg-base-200 shadow-xl">
+          <figure>
+            <img className="h-[250px] w-[250px]" src={avatar} alt="Movie" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title text-2xl text-orange-400 underline decoration-orange-400/30 underline-offset-2">
+              {login}
+            </h2>
+            <p className="leading-6">
+              <span>Blog: </span>
+              {blog ? (
+                <a
+                  className="link link-primary text-orange-400 underline decoration-orange-400/30 underline-offset-1"
+                  href={blog}
+                >
+                  {blog}
+                </a>
+              ) : (
+                <span className="text-slate-400">No Blog</span>
+              )}
+              <br />
+              <span>Twitter: </span>
+              {twitter_username ? (
+                <a
+                  className="link link-primary  text-orange-400 underline decoration-orange-400/30 underline-offset-1"
+                  href={`https://twitter.com/${twitter_username}`}
+                >
+                  @{twitter_username}
+                </a>
+              ) : (
+                <span className="text-slate-400">NO twitter</span>
+              )}
+              <br />
+              <span>Followers: </span>
+              <span>{followers}</span>
+              <br />
+              <span>Following: </span>
+              <span>{following}</span>
+            </p>
+            <div className="card-actions justify-end">
+              <a
+                href={`https://github.com/${login}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <button className="btn btn-outline btn-primary">
+                  Github Profile
+                </button>
+              </a>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 export default Card;
